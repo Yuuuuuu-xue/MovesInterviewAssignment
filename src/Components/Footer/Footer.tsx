8c2ffa48154e9905c3914804c180bc94f157c946
Yuuuuuu-xue <yuxue010625@gmail.com>
2021-11-10 00:44:05 -0500

    remove module styles

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -1,18 +1,17 @@
 import React from "react";
-import style from "../styles/Footer.module.scss";
 import GitHubIcon from '@mui/icons-material/GitHub';
 import IconButton from '@mui/material/IconButton';
-import ButtonStyle from "../styles/Button.module.scss";
+import "./Footer.scss";
 
 interface Props {
 };
 
 const Footer: React.FC<Props> = (): React.ReactElement => {
   return (
-    <footer className={style.footer}>
+    <footer className="footer">
       Yu Xue ©2021 <span>
         <a target="_blank" href="https://github.com/Yuuuuuu-xue">
-          <IconButton className={ButtonStyle.github}>
+          <IconButton className="github">
             <GitHubIcon />
           </IconButton>
         </a>
