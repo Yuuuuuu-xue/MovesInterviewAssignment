6d238a265b6e6ce62056dee25278a2a86bd81c5d
Yuuuuuu-xue <yuxue010625@gmail.com>
2021-11-11 00:54:24 -0500

    Fill the info for DailyWeatherCard

--- src/Components/Cards/DailyWeatherCard/DailyWeatherCard.tsx
+++ src/Components/Cards/DailyWeatherCard/DailyWeatherCard.tsx
@@ -1,6 +1,6 @@
-import React from "react";
+import React, {useState} from "react";
 import { styled } from "@mui/material/styles"; 
-import { Card, CardHeader, CardActions, Collapse, IconButton, Typography} from "@mui/material";
+import { Card, CardHeader, CardContent, CardActions, Collapse, IconButton, Typography} from "@mui/material";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import { DailyWeatherData } from "../../../types/weatherInterface";
 import moment from "moment";
@@ -19,13 +19,71 @@ const ExpandMore = styled((props: any) => {
 
 
 const DailyWeatherCard: React.FC<DailyWeatherData> = ({
-  dt, 
+  dt, temp, clouds, humidity, pressure, wind_speed, rain, snow
 }): React.ReactElement => {
+
+  const [expand, setExpand] = useState(false);
+
+  const handleClick = (): void => {
+    setExpand(!expand); 
+  }
+
   return(
     <Card sx={{maxWidth: 345}}>
       <CardHeader
         title={moment(new Date(dt * 1000)).format('dddd')}
       />
+      <CardContent>
+        <Typography variant="h6">
+          Temperature:
+        </Typography>
+        <Typography paragraph>
+          Day: {temp.day} &deg;C
+          <br />
+          Night: {temp.env} &deg;C
+          <br />
+          Max: {temp.max} &deg;C
+          <br />
+          Min: {temp.min} &deg;C
+        </Typography>
+      </CardContent>
+
+      {/* The click button */}
+      <CardActions disableSpacing>
+        <ExpandMore expand={expand} onClick={handleClick} aria-expanded={expand} aria-label="show more data">
+          <ExpandMoreIcon />
+        </ExpandMore>
+      </CardActions>
+
+      {/* Expand output */}
+      <Collapse in={expand} timeout="auto" unmountOnExit>
+        <CardContent>
+        <p>
+            <span>Cloudiness: </span>
+            {clouds} %
+          </p>
+          <p>
+            <span>Humidity: </span>
+            {humidity} %
+          </p>
+          <p>
+            <span>Pressure: </span>
+            {pressure} hPa
+          </p>
+          <p>
+            <span>Wind Speed: </span>
+            {wind_speed} metre/sec
+          </p> 
+          <p>
+            <span>Rain: </span>
+            {rain ? rain : 0} mm
+          </p>
+          <p>
+            <span>Snow: </span>
+            {snow ? snow : 0} mm
+          </p>
+        </CardContent>
+      </Collapse>
     </Card>
   );
 };
